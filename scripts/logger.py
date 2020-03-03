56f9a9ec944e4fb97414e84b3a0c763e1fdb6e43
jy90 <15900933613@163.com>
2020-03-03 12:30:20 +0800

    logging module

--- scripts/logger.py
+++ scripts/logger.py
@@ -1,33 +1,56 @@
-# coding: utf-8
 import logging
+from logging import handlers
 
 
-class Logger:
-    def __init__(self, path, level=logging.DEBUG):
-        self.logger = logging.getLogger(path)
-        self.logger.setLevel(level)
-        fmt = logging.Formatter('[%(asctime)s] [%(levelname)s] %(filename)s[%(lineno)d] %(message)s', '%Y-%m-%d %H:%M:%S')
-        fh = logging.FileHandler(path)
-        fh.setFormatter(fmt)
-        fh.setLevel(level)
+class Logger(object):
+    kv = {
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+        'critical': logging.CRITICAL
+    }
+
+    def __init__(self, filename, level='info', when='D', backCount=7,
+            fmt='%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s'):
+        self.logger = logging.getLogger(filename)
+        self.logger.setLevel(self.kv.get(level))
+        fmt_str = logging.Formatter(fmt)
+
+        sh = logging.StreamHandler()  # 标准输出
+        sh.setFormatter(fmt_str)
+
+        fh = handlers.TimedRotatingFileHandler(filename=filename,
+                when=when, backupCount=backCount, encoding='utf-8')
+        fh.setFormatter(fmt_str)
+
+        self.logger.addHandler(sh)
         self.logger.addHandler(fh)
 
-    def debug(self, message):
-        self.logger.debug(message)
 
-    def info(self, message):
-        self.logger.info(message)
-
-    def warnning(self, message):
-        self.logger.warn(message)
-
-    def error(self, message):
-        self.logger.error(message)
-
-    def critical(self, message):
-        self.logger.critical(message)
+log = Logger('app.log', level='debug').logger
 
 
-if __name__ == '__main__':
-    logger = Logger('aap.log', logging.ERROR)
-    logger.error('hahaha error msg')
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def socre(self):
+        return self.__score
+
+    @socre.setter
+    def score(self, score):
+        if isinstance(score, int):
+            self.__score = score
+            log.info(f'name: {self.name}, score: {score}')
+        else:
+            log.error(f'student\'s score is not type of int. {str(type(score))}.')
+            raise TypeError(f'the type of score is {str(type(score))}, not int.')
+
+
+if __name__ == "__main__":
+    zhangsan = Student('zhangsan')
+    zhangsan.score = 90
+    wangwu = Student('wangwu')
+    wangwu.score = 86.5
